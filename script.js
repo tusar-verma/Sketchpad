084e4a9f004491bb9a1c55f10781b3d5ef0d5f70
Tusar Verma <tusarverma12345@gmail.com>
2021-06-08 10:16:22 -0300

    Fixed bug: first click does not paint on grid

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const gridcontainer = document.querySelector(".gridContainer");
-const body = document.querySelector("body");
 let isMouseDown = false;
 let color= "rgb(0,0,0)";
 
-body.addEventListener("mousedown", (e) => isMouseDown = true)
-body.addEventListener("mouseup", (e) => isMouseDown = false)
+window.addEventListener("mousedown", (e) => {
+    isMouseDown = true;
+    draw(e);
+})
+window.addEventListener("mouseup", (e) => isMouseDown = false)
 
 for (let index = 0; index < 16; index++) {
     for (let index = 0; index < 16; index++) {
         const gridelement = document.createElement("div");
         gridelement.classList.add("gridElement");
-        gridelement.onmouseenter = () => {
-            if (isMouseDown) {
-                clearSelection();
-                gridelement.style.backgroundColor = color;
-            }
-        }
+        gridelement.addEventListener("mouseenter", draw);
+        gridelement.addEventListener("mousedown", draw);
         gridcontainer.appendChild(gridelement);        
     }
 }
 
+function draw(e) {
+    if (e.target.className == "gridElement"){
+        if (isMouseDown) {
+            clearSelection();
+            e.target.style.backgroundColor = color;
+        }
+    }
+}
 
 function reset(){
     const gridElements = document.querySelectorAll(".gridElement");
@@ -28,6 +34,9 @@ function reset(){
     });
 }
 
+// source: https://stackoverflow.com/questions/62224280/strange-and-unexpected-behaviour-with-javascript-mousedown-mouseup-and-mouseov
+// fix bug that when you hold click on an element the browser recognize that you are 
+// dragging it, not allowing to paint in the grid
 function clearSelection() {
     if (window.getSelection) {window.getSelection().removeAllRanges();}
     else if (document.selection) {document.selection.empty();}
